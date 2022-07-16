92936fe325f64c797dbed9271f67a75ff6dbe124
vicrocsal <victor.rocasalcedo@gmail.com>
2022-07-15 20:08:52 -0500

    added variables of the app

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+*
+*
+*
+*/
+
+/*
+* Función que contiene todas las propiedades y métodos de la aplicación
+*
+*
+*/
+let game = () =>{
+    let time = 30;
+    let movement = 20;
+    let movementBar = 20;
+    let width = document.documentElement.clientWidth - movement;
+    let height = document.documentElement.clientHeight - movement;
+    let controlGame;
+    let player1;
+    let player2;
+
+
+
+
+}
