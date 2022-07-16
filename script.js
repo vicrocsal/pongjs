af3017243434382be180166fcb30b6eadbef6ecd
vicrocsal <victor.rocasalcedo@gmail.com>
2022-07-15 20:47:49 -0500

    Se agregan funciones moveBar para controlar el movimiento de las barras

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ let game = () => {
   let player2;
 
   /*
- *
- *Función que inicia el juego
- *
- */
+   *
+   *Función que inicia el juego
+   *
+   */
 
   let start = () => {
     init();
@@ -31,10 +31,10 @@ let game = () => {
   };
 
   /*
- *
- *Función que establece los estados iniciales del juego
- *
- */
+   *
+   *Función que establece los estados iniciales del juego
+   *
+   */
 
   let init = () => {
     ball.style.left = 0;
@@ -46,22 +46,83 @@ let game = () => {
     player1.keyCode = null;
     player2.keyPress = false;
     player2.keyCode = null;
-
   };
 
+  /*
+   *
+   * Función que termina el juego
+   *
+   */
   let stop = () => {
     clearInterval(controlGame);
     document.body.style.background = "#f00";
+  };
 
-  }
+  /*
+   *
+   *Función que controla el juego
+   *
+   */
 
   let play = () => {
-    
-  }
+    moveBar();
+  };
+
+   /*
+   *
+   *Función que detecta el movimiento de la barras y las controla
+   *
+   */
+
+  let moveBar = () => {
+    if (player1.keyPress) {
+      if (player1.keyCode == 81 && bar1.offsetTop >= 0)
+        bar1.style.top = bar1.offsetTop - movementBar + "px";
+      if (player1.keyCode == 65 && bar1.offsetTop + bar1.clientHeight <= height)
+        bar1.style.top = bar1.offsetTop + movementBar + "px";
+    }
+    if (player2.keyPress) {
+      if (player2.keyCode == 79 && bar2.offsetTop >= 0)
+        bar2.style.top = bar2.offsetTop - movementBar + "px";
+      if (player2.keyCode == 76 && bar2.offsetTop + bar2.clientHeight <= height)
+        bar2.style.top = bar2.offsetTop + movementBar + "px";
+    }
+  };
+
+  /*
+   *
+   *Función que controla el movimiento de las barras hacia abajo
+   *
+   */
+
+  document.onkeydown = function (e) {
+    e = e || window.event;
+    switch (e.keyCode) {
+      case 81:
+      case 65:
+        player1.keyCode = e.keyCode;
+        player1.keyPress = true;
+        break;
+      case 79:
+      case 76:
+        player2.keyCode = e.keyCode;
+        player2.keyPress = true;
+        break;
+    }
+  };
+
+  /*
+   *
+   *Función que controla el movimiento de las barras hacia arriba
+   *
+   */
+
+  document.onkeyup = function (e) {
+    if (e.keyCode == 81 || e.keyCode == 65) player1.keyPress = false;
+    if (e.keyCode == 79 || e.keyCode == 76) player2.keyPress = false;
+  };
 
   start();
-
-
 };
 
 game();
